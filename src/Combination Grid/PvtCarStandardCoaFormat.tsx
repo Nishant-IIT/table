866d357986009638216d81655eb270dabf38a649
Nishant <nishantbhagat435@gmail.com>
2023-08-20 14:24:54 +0530

    ready to multiply

--- src/Combination Grid/PvtCarStandardCoaFormat.tsx
+++ src/Combination Grid/PvtCarStandardCoaFormat.tsx
@@ -2,22 +2,32 @@ import { HotTable } from '@handsontable/react';
 import { registerAllModules } from 'handsontable/registry';
 import 'handsontable/dist/handsontable.full.min.css';
 import { pvtCarStandardCOAFormat } from '../Data/Data';
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 // register Handsontable's modules  
 registerAllModules();
 
 function Home() {
     const hotRef = useRef(null);
+    const [output, setOutput] = useState('Data will load from server');
+    const [isAutosave, setIsAutosave] = useState(false);
 
     let buttonClickCallback;
+    let saveClickCallback;
+
+    const autosaveClickCallback = (event) => {
+        setIsAutosave(event.target.checked);
+        if (event.target.checked) {
+            setOutput('Changes will be autosaved');
+        } else {
+            setOutput('Changes will not be autosaved');
+        }
+    };
 
     useEffect(() => {
-        const hot = hotRef.current?.hotInstance;
+        const hot = hotRef.current.hotInstance;
 
-        
-        if(hot){
-            const exportPlugin = hot.getPlugin('exportFile');
+        const exportPlugin = hot.getPlugin('exportFile');
         // eslint-disable-next-line react-hooks/exhaustive-deps
         buttonClickCallback = () => {
             exportPlugin.downloadFile('csv', {
@@ -33,7 +43,22 @@ function Home() {
                 rowHeaders: true
             });
         };
-    }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        saveClickCallback = () => {
+            // save all cell's data
+            fetch('https://handsontable.com/docs/scripts/json/save.json', {
+                method: 'POST',
+                mode: 'no-cors',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ data: hot.getData() })
+            })
+                .then(response => {
+                    setOutput('Data saved');
+                    console.log('The POST request is only used here for the demo purposes');
+                });
+        };
     });
     return (
         <>
@@ -43,16 +68,15 @@ function Home() {
                 rowHeaders={true}
                 colHeaders={['Channel', 'Sub-Channel', 'Branch Location', 'Location Category', 'LOB', '(Product)', 'Business Type', 'Make', 'Model', 'Cubic Capacity', 'Segment', 'VehicleClass', 'Fuel Type', 'Section Text ', 'RTO State', 'RTO', 'With CPA', 'NCB', 'Vehicle Age Cat', 'Lower Discount', 'Upper Discount', 'Business Slab', 'Approval Grid for OD Portion', 'Approval Grid for TP Portion', 'Approval Grid for Per Policy ']}
                 height="auto"
-                // height={320}
                 fixedRowsTop={1}
                 licenseKey="non-commercial-and-evaluation" // for non-commercial use only
-                //className="htCenter"
                 contextMenu={true}
                 filters={true}
                 comments={true}
                 columnSorting={true}
                 dropdownMenu={['filter_by_condition', 'filter_by_value', 'filter_action_bar']}
-                // customBorders={true}
+                customBorders={true}
+
                 columns={[
                     {
                         "data": "Channel",
@@ -164,6 +188,14 @@ function Home() {
             <div className="controls">
             <button id="export-file" onClick={(...args) => buttonClickCallback(...args)}>Download</button>
             </div>
+            <div className="controls">
+                <button id="save" className="button button--primary button--blue" onClick={(...args) => saveClickCallback(...args)}>Save data</button>
+                <label>
+                    <input type="checkbox" name="autosave" id="autosave" checked={isAutosave} onClick={(...args) => autosaveClickCallback(...args)} />
+                    Autosave
+                </label>
+            </div>
+            <output className="console" id="output">{output}</output>
         </>
     );
 }
---
+++ src/Combination Grid/a.tsx
@@ -0,0 +1,97 @@
+import { HotTable } from '@handsontable/react';
+import { registerAllModules } from 'handsontable/registry';
+import 'handsontable/dist/handsontable.full.min.css';
+import { pvtCarStandardCOAFormat } from '../Data/Data';
+import React, { useEffect, useRef, useState } from 'react';
+
+// register Handsontable's modules  
+registerAllModules();
+
+function Home() {
+    const hotRef = useRef(null);
+    const [output, setOutput] = useState('Data will load from server');
+    const [isAutosave, setIsAutosave] = useState(false);
+
+    let buttonClickCallback;
+    let saveClickCallback;
+
+    const autosaveClickCallback = (event) => {
+        setIsAutosave(event.target.checked);
+        if (event.target.checked) {
+            setOutput('Changes will be autosaved');
+        } else {
+            setOutput('Changes will not be autosaved');
+        }
+    };
+
+    useEffect(() => {
+        const hot = hotRef.current.hotInstance;
+
+        const exportPlugin = hot.getPlugin('exportFile');
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        buttonClickCallback = () => {
+            exportPlugin.downloadFile('csv', {
+                bom: false,
+                columnDelimiter: ',',
+                columnHeaders: false,
+                exportHiddenColumns: true,
+                exportHiddenRows: true,
+                fileExtension: 'csv',
+                filename: 'Handsontable-CSV-file_[YYYY]-[MM]-[DD]',
+                mimeType: 'text/csv',
+                rowDelimiter: '\r\n',
+                rowHeaders: true
+            });
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        saveClickCallback = () => {
+            // save all cell's data
+            fetch('https://handsontable.com/docs/scripts/json/save.json', {
+                method: 'POST',
+                mode: 'no-cors',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ data: hot.getData() })
+            })
+                .then(response => {
+                    setOutput('Data saved');
+                    console.log('The POST request is only used here for the demo purposes');
+                });
+        };
+    });
+    return (
+        <>
+            <HotTable
+                ref={hotRef}
+                data={pvtCarStandardCOAFormat}
+                rowHeaders={true}
+                colHeaders={[]}                
+                height="auto"
+                fixedRowsTop={1}
+                licenseKey="non-commercial-and-evaluation" // for non-commercial use only
+                contextMenu={true}
+                filters={true}
+                comments={true}
+                columnSorting={true}
+                dropdownMenu={['filter_by_condition', 'filter_by_value', 'filter_action_bar']}
+                customBorders={true}
+
+                columns={[]
+                }
+            />
+            <div className="controls">
+            <button id="export-file" onClick={(...args) => buttonClickCallback(...args)}>Download</button>
+            </div>
+            <div className="controls">
+                <button id="save" className="button button--primary button--blue" onClick={(...args) => saveClickCallback(...args)}>Save data</button>
+                <label>
+                    <input type="checkbox" name="autosave" id="autosave" checked={isAutosave} onClick={(...args) => autosaveClickCallback(...args)} />
+                    Autosave
+                </label>
+            </div>
+            <output className="console" id="output">{output}</output>
+        </>
+    );
+}
+export default Home;
